b306577a27a71556ae2aa6245f5efd53e6cebfe1
carlosandres.munoz <carlosandres.munoz@globant.com>
2022-01-31 05:09:37 -0300

    deleted variables without use.

--- app/src/main/java/com/globant/carlosmunoz/myweather/utils/Constants.kt
+++ app/src/main/java/com/globant/carlosmunoz/myweather/utils/Constants.kt
@@ -3,9 +3,5 @@ package com.globant.carlosmunoz.myweather.utils
 object Constants {
 
     const val BASE_URL = "https://api.openweathermap.org"
-    const val API_KEY = "feea94abd0348b78768772ca47548986"
-
-    var QUERY_LON = ""
-    var QUERY_LAT = ""
-
+    const val API_KEY = ""
 }
